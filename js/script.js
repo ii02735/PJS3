cce780843c2c8a9e86849c4f34e4a7df8fd3279c
ii02735 <muhammad.yadallee@etu.paridescartes.fr>
2017-11-21 22:43:44 +0100

    Slideshow mis à jour

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
-var indiceSlide = 2; //On affiche dès le lancement de notre page, la première slide
+/*document.getElementsBy... -> Document Object Model : le JS capture les propriétés du CSS
+pour changer ses propriétés par la suite, c'est ce qui rend un site dynamique depuis le client*/
+
+var slides = document.getElementsByClassName("slides"); //On capture toutes nos classes/slides
+var dots = document.getElementsByClassName("point");
+
+var indiceSlide = 1; //On affiche dès le lancement de notre page, la première slide
+
 afficherSlide(indiceSlide);
 
-function incrSlide(indice_){
-	afficherSlide(indiceSlide += indice_);
+function incrIndice()
+{
+	alert(indiceSlide);
+	indiceSlide++; 
+	afficherSlide(indiceSlide%(slides.length));
 }
 
 function SlideActuelle(indice_){
 	afficherSlide(indiceSlide = indice_);
 }
 
-/*document.getElementsBy... -> Document Object Model : le JS capture les propriétés du CSS
-pour changer ses propriétés par la suite, c'est ce qui rend un site dynamique depuis le client*/
 function afficherSlide(indice_){
-	var i;
-	var slides = document.getElementsByClassName("slides"); //On capture toutes nos classes/slides
-	var dots = document.getElementsByClassName("point");
-	if(indice_ > slides.length) //Si on atteint la fin de nos slides
-		indiceSlide = 1; //On revient au début
-	for(i = 0; i<slides.length;i++)
+
+	for(var i = 0; i<slides.length;i++)
 		slides[i].style.display = "none"; //On masque les slides en plus pour n'en garder qu'une seule
-	for (i = 0; i < dots.length; i++)
-      dots[i].className = dots[i].className.replace(" active", "");
-	slides[indiceSlide-1].style.display = "block"; //On affiche une unique slide
-	dots[indiceSlide-1].className += " active";
+	for (var i = 0; i < dots.length; i++)
+      dots[i].className = dots[i].className.replace(" active", "");	
+	//Pour varier le texte en fonction de la slide
+	var titreSlide = document.getElementById("texteDyn");
+	titreSlide.style.fontWeight="bold";
+	switch(indice_)
+	{
+		case 1:
+			slides[indice_-1].style.display = "block"; //On affiche une unique slide
+			dots[indiceSlide-1].className += " active";
+			titreSlide.textContent="Activités";
+			titreSlide.style.color="black";
+			break;
+		case 2:
+			slides[indice_-1].style.display = "block"; //On affiche une unique slide
+			dots[indiceSlide-1].className += " active";
+			titreSlide.textContent="Transports";
+			titreSlide.style.color="white";
+			break;
+		case 3:
+			slides[indice_-1].style.display = "block"; //On affiche une unique slide
+			dots[indiceSlide-1].className += " active";
+			titreSlide.textContent="Hébergement";
+			titreSlide.style.color="white";
+			break;
+		case 4:
+			slides[indice_-1].style.display = "block"; //On affiche une unique slide
+			dots[indiceSlide-1].className += " active";
+			titreSlide.textContent="Gastronomie";
+			titreSlide.style.color="white";
+			break;
+	}
+	
 }
