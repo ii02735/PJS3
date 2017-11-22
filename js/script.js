cbc846bff2446336383b468d6357c144a7c55053
ii02735 <muhammad.yadallee@etu.paridescartes.fr>
2017-11-22 22:56:07 +0100

    Slideshow avec corrections terminées

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ pour changer ses propriétés par la suite, c'est ce qui rend un site dynamique 
 
 var slides = document.getElementsByClassName("slides"); //On capture toutes nos classes/slides
 var dots = document.getElementsByClassName("point");
+var indiceSlide = 0; //On affiche dès le lancement de notre page, la première slide
+var timer = setInterval(incrPicture,5000);//Toutes les 5 secondes, la prochaine slide sera affichée
 
-var indiceSlide = 1; //On affiche dès le lancement de notre page, la première slide
+SlideSelect(indiceSlide);
 
-afficherSlide(indiceSlide);
-
-function incrIndice()
+function SlideSelectedByUser(indice_)//S'il y a clic de l'utilisateur, le timer se remet à 0
 {
-	alert(indiceSlide);
-	indiceSlide++; 
-	afficherSlide(indiceSlide%(slides.length));
+	clearInterval(timer);
+	timer = setInterval(incrPicture,5000);
+	SlideSelect(indice_);
 }
 
-function SlideActuelle(indice_){
+function SlideSelect(indice_){
+	
 	afficherSlide(indiceSlide = indice_);
 }
 
@@ -28,32 +29,83 @@ function afficherSlide(indice_){
 	//Pour varier le texte en fonction de la slide
 	var titreSlide = document.getElementById("texteDyn");
 	titreSlide.style.fontWeight="bold";
+
 	switch(indice_)
 	{
-		case 1:
-			slides[indice_-1].style.display = "block"; //On affiche une unique slide
-			dots[indiceSlide-1].className += " active";
+		case 0:
+			slides[indice_].style.display = "block"; //On affiche une unique slide
+			dots[indice_].className += " active";
 			titreSlide.textContent="Activités";
 			titreSlide.style.color="black";
 			break;
-		case 2:
-			slides[indice_-1].style.display = "block"; //On affiche une unique slide
-			dots[indiceSlide-1].className += " active";
+		case 1:
+			slides[indice_].style.display = "block"; //On affiche une unique slide
+			dots[indice_].className += " active";
 			titreSlide.textContent="Transports";
 			titreSlide.style.color="white";
 			break;
-		case 3:
-			slides[indice_-1].style.display = "block"; //On affiche une unique slide
-			dots[indiceSlide-1].className += " active";
+		case 2:
+			slides[indice_].style.display = "block"; //On affiche une unique slide
+			dots[indice_].className += " active";
 			titreSlide.textContent="Hébergement";
 			titreSlide.style.color="white";
 			break;
-		case 4:
-			slides[indice_-1].style.display = "block"; //On affiche une unique slide
-			dots[indiceSlide-1].className += " active";
+		case 3:
+			slides[indice_].style.display = "block"; //On affiche une unique slide
+			dots[indice_].className += " active";
 			titreSlide.textContent="Gastronomie";
 			titreSlide.style.color="white";
 			break;
 	}
+}
+
+function incrPicture()
+{
+	indiceSlide++;
+	SlideSelect(indiceSlide%(slides.length));
+	//alert("Modulo : " + indiceSlide + "%" + slides.length + " = " + indiceSlide%(slides.length));
+	/*La technique du modulo
+		
+		Le reste nous permet de ne pas dépasser les dimensions du tableau.
+		Supposons que notre tableau est de longueur 4 => {0,1,2,3}
+		Exemples :
+		0 mod 4 = 0
+		1 mod 4 = 1
+		2 mod 4 = 2
+		3 mod 4 = 3
+		4 mod 4 = 0 --> On revient au début du tableau (il n'y a aucun reste)
+		5 mod 4 = 1
+		6 mod 4 = 2
+		7 mod 4 = 3
+		8 mod 4 = 0 --> On revient au début du tableau
+		et ainsi de suite...
+	*/
+}
+
+function minusSlide()
+{
+	clearInterval(timer);
+	timer = setInterval(incrPicture,5000);
+	indiceSlide--;
+	/*En mathématiques
+	  -1 mod 4 = 3
+	  Or en JS
+	  -1 mod 4 = -1
+	  On corrige cette erreur avec negativeTransform
+	  Exemple :
+	  -1 mod 4 = -1
+	  -->negativeTransform = -1 + slides.length = 3
+	*/
+	var negativeTransform = (indiceSlide < 0)?(slides.length)+indiceSlide%(slides.length):indiceSlide%(slides.length);
+	SlideSelect(negativeTransform);
 	
 }
+
+function plusSlide()
+{
+	clearInterval(timer);
+	timer = setInterval(incrPicture,5000);
+	indiceSlide++;
+	SlideSelect(indiceSlide%(slides.length));
+}
+
